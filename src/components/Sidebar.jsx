6e9809e7773727b4acee8b45d1ec8585bd6ad562
nischalad <nischaladhikari2034@gmail.com>
2025-05-13 00:19:01 +0545

    Add Home and Login page

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -8,11 +8,17 @@ const Sidebar = ({selectedUser, setSelectedUser}) => {
     <div className={`bg-[#8185B2]/10 h-full p-5 rounded-r-xl overflow-y-scroll text-white ${selectedUser ? "max-md:hidden" : ''} `}>
       <div className='pb-5'>
         <div className='flex justify-between items-center'>
-            <img src={assets.logo} alt="logo" className='max-w-40' />
+            <img src={assets.logo} 
+              alt="logo" 
+              className='max-w-40' />
             <div className='relative py-2 group'>
-                <img src={assets.menu_icon} alt="menu" className='max-h-5 cursor-pointer' />
+                <img src={assets.menu_icon} 
+                  alt="menu" 
+                  className='max-h-5 cursor-pointer' />
                 <div className='absolute top-full right-0 z-20 w-32 p-5 rounded-md bg-[#282142] border-gray-600 text-gray-100 hidden group-hover:block'>
-                    <p onClick={()=>navigate('/profile')} className='cursor-pointer text-sm'>Edit Profile</p>
+                    <p 
+                      onClick={()=>navigate('/profile')} 
+                      className='cursor-pointer text-sm'>Edit Profile</p>
                     <hr className='my-2 border-t border-gray-500'/>
                     <p className='cursor-pointer text-sm'>Logout</p>
                 </div>
@@ -32,7 +38,10 @@ const Sidebar = ({selectedUser, setSelectedUser}) => {
                 <div
                     onClick={()=> {setSelectedUser(user)}} 
                     key={index} 
-                    className={`relative flex items-center gap-2 p-2 pl-4 rounded cursor-pointer max-sm:text-sm ${selectedUser?._id === user._id && 'bg-[#282142]/50'}`}>
+                    className={`relative flex items-center gap-2 p-2 pl-4 rounded cursor-pointer max-sm:text-sm
+                     ${selectedUser
+                     ?._id === user._id 
+                     && 'bg-[#282142]/50'}`}>
                    <img src={user?.profilePic || assets.avatar_icon} alt="" className='w-[35px] aspect-[1/1] rounded-full' /> 
                    <div className='flex flex-col leading-5'>
                     <p>{user.fullName}</p>
