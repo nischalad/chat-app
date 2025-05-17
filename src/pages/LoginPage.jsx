d9cd910fd004c1126c4ec5ba7a932f489cef41ca
nischalad <nischaladhikari2034@gmail.com>
2025-05-17 23:49:15 +0545

    Profile page

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -8,7 +8,15 @@ const LoginPage = () => {
   const [password, setPassword] = useState("")
   const [bio, setBio] = useState("")
   const [isDataSubmitted, setIsDataSubmitted] = useState(false);
-
+  
+  const onSubmitHandler=(event)=>{
+    event.preventDefault();
+   
+    if(currState === "Sign up" && !isDataSubmitted ){
+      setIsDataSubmitted(true)
+      return;
+    }
+  }
 
   return (
     <div className='min-h-screen bg-cover bg-center flex items-center justify-between gap-8 sm:justify-evenly max-sm:flex-col backdrop-blur-2xl '>
@@ -19,13 +27,15 @@ const LoginPage = () => {
           className='w-[min(30vw,250px)]' />
       
       {/* right */}
-      <form className='border-2 bg-white/8 text-white border-gray-500 p-6 flex flex-col gap-6 rounded-lg shadow-lg'>
+      <form onSubmit={onSubmitHandler} className='border-2 bg-white/8 text-white border-gray-500 p-6 flex flex-col gap-6 rounded-lg shadow-lg'>
         <h2 className='font-medium text-2xl flex items-center justify-between'>
           {currState}
-          <img 
-            src={assets.arrow_icon} 
-            alt="" 
-            className='w-5 cursor-pointer' />
+          {isDataSubmitted &&         
+            <img 
+              onClick={()=>setIsDataSubmitted(false)}
+              src={assets.arrow_icon} 
+              alt="" 
+              className='w-5 cursor-pointer' /> }
         </h2>
 
         {currState==="Sign up" && !isDataSubmitted &&(
@@ -59,7 +69,7 @@ const LoginPage = () => {
 
         {currState === "Sign up" && isDataSubmitted && (
           <textarea 
-            onChange={(e)=>(e.target.value)} 
+            onChange={(e) => setBio(e.target.value)} 
             value={bio}
             rows={4} 
             className='p-2 border border-gray-500 rounded-md focus:outline-none focus:rin-2 focus:ring-indigo-500'
@@ -70,7 +80,7 @@ const LoginPage = () => {
 
         <button 
           type='submit'
-          className='py-3 bg-gradient-to-r from-purple-400 to-violet-600 text-whote rounded-md cursor-pointer'>
+          className='py-3 bg-gradient-to-r from-purple-400 to-violet-600 text-white rounded-md cursor-pointer'>
           {currState==="Sign up" ? "Create Account" : "Log in"}
         </button>
 
